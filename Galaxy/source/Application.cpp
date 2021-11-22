e32c315e2cda5e3fce36f4d8c5a9c25d03957c98
Tiwann <tiwannbeats@gmail.com>
2021-11-22 21:28:11 +0100

    GLFW Init test

--- Galaxy/source/Application.cpp
+++ Galaxy/source/Application.cpp
@@ -1,5 +1,12 @@
-#include "Shader/Shader.h"
+#include "GLFW/glfw3.h"
+#include <iostream>
 
 int main() {
+    if (!glfwInit()) {
+        std::cout << "Couldn't initalize GLFW." << std::endl;
+        return -1;
+    }
+
+
     return 0;
 }
