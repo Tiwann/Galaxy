7841a7f138f7d7017f026f3b062dea7b71d46aea
Tiwann <tiwannbeats@gmail.com>
2021-11-23 00:24:49 +0100

    Worked on clean scripts

--- clean.bat
+++ clean.bat
@@ -1,31 +1,19 @@
 @echo off
 
-if exist binaries (
-    echo Deleting binaries directory
-    rmdir /s /q binaries
-)
-
-if exist intermediate (
-    echo Deleting intermediate directory
-    rmdir /s /q intermediate
-)
-
-if exist .vs (
-    echo Deleting .vs directory
-    rmdir /s /q .vs
-)
-
-call Vendor/GLFW/clean.bat
-
 if exist Galaxy.sln del Galaxy.sln
-if exist Galaxy\Galaxy.vcxproj del Galaxy\Galaxy.vcxproj
-if exist Galaxy\Galaxy.vcxproj.user del Galaxy\Galaxy.vcxproj.user
-if exist Galaxy\Galaxy.vcxproj.filters del Galaxy\Galaxy.vcxproj.filters
 
-if exist Vendor\GLFW\GLFW.vcxproj del Vendor\GLFW\GLFW.vcxproj
-if exist Vendor\GLFW\GLFW.vcxproj.user del Vendor\GLFW\GLFW.vcxproj.user
-if exist Vendor\GLFW\GLFW.vcxproj.filters del Vendor\GLFW\GLFW.vcxproj.filters
+echo Deleted Galaxy solution!
 
-echo Cleaned project directory!
+cd Galaxy
+call clean.bat
+cd ..\
+
+cd Vendor/GLFW
+call clean.bat
+cd ..\..\
+
+cd Vendor/GLEW
+call clean.bat
+cd ..\..\
 
 PAUSE
