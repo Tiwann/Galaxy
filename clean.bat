17df5a69d6dfdde7ec38644b9b3623e49240e1d3
Tiwann <tiwannbeats@gmail.com>
2021-11-22 21:03:58 +0100

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -14,10 +14,15 @@ if exist .vs (
     echo Deleting .vs directory
     rmdir /s /q .vs
 )
+
 if exist Galaxy.sln del Galaxy.sln
 if exist Galaxy\Galaxy.vcxproj del Galaxy\Galaxy.vcxproj
 if exist Galaxy\Galaxy.vcxproj.user del Galaxy\Galaxy.vcxproj.user
 if exist Galaxy\Galaxy.vcxproj.filters del Galaxy\Galaxy.vcxproj.filters
+
+if exist Vendor\GLFW\GLFW.vcxproj del Vendor\GLFW\GLFW.vcxproj
+if exist Vendor\GLFW\GLFW.vcxproj.user del Vendor\GLFW\GLFW.vcxproj.user
+if exist Vendor\GLFW\GLFW.vcxproj.filters del Vendor\GLFW\GLFW.vcxproj.filters
 
 echo Cleaned project directory!
 
