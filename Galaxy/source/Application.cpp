ef6063ec6d5f075f0e19ccb03f93b2704230d409
Tiwann <tiwannbeats@gmail.com>
2021-11-22 22:02:08 +0100

    Log class test

--- Galaxy/source/Application.cpp
+++ Galaxy/source/Application.cpp
@@ -1,11 +1,16 @@
-#include "GLFW/glfw3.h"
-#include <iostream>
+#include <GLFW/glfw3.h>
+#include "Log/Log.h"
+
 
 int main() {
+    Log::Init();
+    
     if (!glfwInit()) {
-        std::cout << "Couldn't initalize GLFW." << std::endl;
+        LOG_INFO("Couldn't initalize GLFW.");
         return -1;
     }
+
+    LOG_INFO("Using GLFW version {}.{}", GLFW_VERSION_MAJOR, GLFW_VERSION_MINOR)
 
 
     return 0;
