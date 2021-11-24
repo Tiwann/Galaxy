6f0f9bd489c9cb006890b9937cb174fef6f605e6
Tiwann <tiwannbeats@gmail.com>
2021-11-24 03:27:29 +0100

    Misc modifications

--- Galaxy/source/Renderer/VertexArray/VertexArray.h
+++ Galaxy/source/Renderer/VertexArray/VertexArray.h
@@ -8,10 +8,11 @@ namespace Galaxy
 		unsigned int ID;
 		VertexArray();
 	public:
-	
 		static VertexArray* Create();
+	
 		void Bind() const;
 		void Unbind() const;
+		//void AddBuffer(const VertexBuffer& vbo, const VertexBufferLayout& layout);
 	};
 }
 
