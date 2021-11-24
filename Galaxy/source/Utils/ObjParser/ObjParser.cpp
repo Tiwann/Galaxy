4d7453f0126d4c4df96d3a9a46e8c1dcd3eff852
Tiwann <tiwannbeats@gmail.com>
2021-11-24 16:28:39 +0100

    Finished working on ObjParser

--- Galaxy/source/Utils/ObjParser/ObjParser.cpp
+++ Galaxy/source/Utils/ObjParser/ObjParser.cpp
@@ -19,7 +19,6 @@ namespace Galaxy
         }
         std::string line;
         std::string content;
-        std::vector<std::string> names;
 
         uint hcount  = 0;
         uint ocount  = 0;
@@ -28,14 +27,13 @@ namespace Galaxy
         uint vncount = 0;
         uint fcount  = 0;
 
+        std::vector<glm::ivec3> tmp_ind = std::vector<glm::ivec3>(3);
         glm::fvec3 tempvec = glm::fvec3(0, 0, 0);
         glm::fvec2 tempvec2 = glm::fvec2(0, 0);
 
-        std::vector<glm::fvec3> pos;
-        std::vector<glm::fvec2> uv;
-        std::vector<glm::fvec3> nrm;
-        std::vector<uint> indices;
-        
+        ObjData data;
+
+        // Begin parse data from obj file
         while (std::getline(stream, line))
         {
             if (line[0] == '#')
@@ -48,7 +46,7 @@ namespace Galaxy
             {
                 ocount++;
                 line = line.substr(2, line.size());
-                names.push_back(line);               
+                data.objects.push_back(line);               
             }
 
             if (line[0] == 'v' && line[1] == ' ')
@@ -56,7 +54,7 @@ namespace Galaxy
                 vcount++;
                 line = line.substr(2, line.size());
                 sscanf_s(line.c_str(), "%f %f %f", &tempvec.x, &tempvec.y, &tempvec.z);
-                pos.push_back(tempvec);
+                data.positions.push_back(tempvec);
             }
 
             if (line[0] == 'v' && line[1] == 't')
@@ -64,7 +62,7 @@ namespace Galaxy
                 vtcount++;
                 line = line.substr(3, line.size());
                 sscanf_s(line.c_str(), "%f %f", &tempvec2.x, &tempvec2.y);
-                uv.push_back(tempvec2);
+                data.texCoords.push_back(tempvec2);
             }
 
             if (line[0] == 's')
@@ -77,23 +75,49 @@ namespace Galaxy
                 vncount++;
                 line = line.substr(3, line.size());
                 sscanf_s(line.c_str(), "%f %f %f", &tempvec.x, &tempvec.y, &tempvec.z);
-                nrm.push_back(tempvec);
+                data.normals.push_back(tempvec);
             }
 
             if (line[0] == 'f')
             {
                 fcount++;
+                line = line.substr(2, line.size());
+
+                sscanf_s(line.c_str(), "%d/%d/%d %d/%d/%d %d/%d/%d", 
+                    &tmp_ind[0].x, &tmp_ind[0].y, &tmp_ind[0].z,
+                    &tmp_ind[1].x, &tmp_ind[1].y, &tmp_ind[1].z,
+                    &tmp_ind[2].x, &tmp_ind[2].y, &tmp_ind[2].z);
+
+                data.indices.push_back(tmp_ind[0]);
+                data.indices.push_back(tmp_ind[1]);
+                data.indices.push_back(tmp_ind[2]);
             }
         }
+        stream.close();
 
-        std::cout << "-------Name:  " << names[0] << std::endl;
-        std::cout << "---Comments:  " << hcount << std::endl;
-        std::cout << "----Objects:  " << ocount << std::endl;
-        std::cout << "---Vertices:  " << vcount << std::endl;
-        std::cout << "--TexCoords:  " << vtcount << std::endl;
-        std::cout << "----Normals:  " << vncount << std::endl;
-        std::cout << "------Faces:  " << fcount << std::endl;
+        //std::cout << "[OBJPARSER]-------Name:  " << names[0] << std::endl;
+        //std::cout << "[OBJPARSER]---Comments:  " << hcount << std::endl;
+        //std::cout << "[OBJPARSER]----Objects:  " << ocount << std::endl;
+        //std::cout << "[OBJPARSER]---Vertices:  " << vcount << std::endl;
+        //std::cout << "[OBJPARSER]--TexCoords:  " << vtcount << std::endl;
+        //std::cout << "[OBJPARSER]----Normals:  " << vncount << std::endl;
+        //std::cout << "[OBJPARSER]------Faces:  " << fcount << std::endl;
+        return data;
+    }
 
-        return {};
+    
+    Vertices ObjParser::DataToVertices(const ObjData objdata)
+    {
+        Vertices vertices = Vertices(objdata.indices.size());
+        for (uint i = 0; i < objdata.indices.size(); i++)
+        {
+            vertices[i] = {
+            .position = objdata.positions[objdata.indices[i].x - 1],
+            .texCoord = objdata.texCoords[objdata.indices[i].y - 1],
+            .normal = objdata.normals[objdata.indices[i].z - 1],
+            .color = glm::fvec4(1.0)
+            };
+        }
+        return vertices;
     }
 }
--- Galaxy/source/Utils/ObjParser/ObjParser.h
+++ Galaxy/source/Utils/ObjParser/ObjParser.h
@@ -4,21 +4,26 @@
 #include <string>
 #include <glm/glm.hpp>
 
+#include "Renderer/Vertex.h"
+
 
 namespace Galaxy 
 {	
+
 	struct ObjData
 	{
+		std::vector<std::string> objects;
 		std::vector<glm::fvec3> positions;
 		std::vector<glm::fvec2> texCoords;
 		std::vector<glm::fvec3> normals;
-
+		std::vector<glm::ivec3> indices;
 	};
 
 	class ObjParser
 	{
 	public:
 		static ObjData ParseFile(const std::string filepath);
+		static Vertices DataToVertices(const ObjData objdata);
 	};
 }
 
